1ed0e9a111c1f70a1eda78fa887d468a522f5d33
Karlitoo <quentin@codebuds.com>
2023-01-11 16:07:52 +0100

    feat: removing authentication

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:web_app/app/app.dart';
 import 'package:web_app/bootstrap.dart';
-import 'package:web_app/firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   await bootstrap(() => const App());
 }
